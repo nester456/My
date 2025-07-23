ca5775665ae27cb020c4aa9b31e0d9ef2ca87ad4
Mariia Nesterenko <nr512@drc.ngo>
2025-07-23 13:43:54 +0300

    виправлено імпорт makeWASocket

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-import makeWASocket, { useMultiFileAuthState, DisconnectReason } from '@whiskeysockets/baileys'
+import { makeWASocket, useMultiFileAuthState, DisconnectReason } from '@whiskeysockets/baileys'
 import { Boom } from '@hapi/boom'
 import qrcode from 'qrcode'
 import TelegramBot from 'node-telegram-bot-api'
